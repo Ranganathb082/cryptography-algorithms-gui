138701cdd04c2a57942883891dc61b054ac4e53d
Ranganath <ranganathb.mca22@rvce.edu.in>
2023-08-19 19:10:22 +0530

    fixed some bugs related to the url path

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -1,8 +1,9 @@
 
 
 from os import path
+from django.urls import path
 from apis.views import hello_world
 
 urlpatterns = [
-    path('hello_world', hello_world, name = hello_world)
+    path('hello_world/', hello_world, name = "hello_world")
 ]
