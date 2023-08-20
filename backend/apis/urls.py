94862358612ad12bdc0501a6153c6e4ee6a4ec3f
Ranganath <ranganathb.mca22@rvce.edu.in>
2023-08-20 13:48:45 +0530

    newly added new views added, playFairEncrypt and playFairDecrypt new urls added, playfairencrypt/, playfairdecrypt/ installed rest_framework and corsheaders using pip tested the encryption and decryption apis with postman

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -3,7 +3,11 @@
 from os import path
 from django.urls import path
 from apis.views import hello_world
+from apis.views import playFairEncrypt
+from apis.views import playFairDecrypt
 
 urlpatterns = [
-    path('hello_world/', hello_world, name = "hello_world")
+    path('hello_world/', hello_world, name = "hello_world"),
+    path('playfairencrypt/', playFairEncrypt, name="playFairEncrypt" ),
+    path('playfairdecrypt/',playFairDecrypt, name="playFairDecrypt" )
 ]
--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,8 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from apis.playfair import *
 
 # Create your views here.
 
 
 def hello_world(requeset):
     return HttpResponse("Hello World")
+
+
+def playFairEncrypt(request):
+    if request.method == 'GET':
+        plain_text = request.GET.get('plain_text', '')
+        key = request.GET.get('key', '')
+
+        encryptResult=playfair(key, plain_text)
+
+        return JsonResponse({'encrypted_string': encryptResult.lower()})
+
+
+def playFairDecrypt(request):
+    if request.method == 'GET':
+        en_plain_text = request.GET.get('en_plain_text', '')
+        # get method takes parameter so use the parameter in the url too
+        key = request.GET.get('key', '')
+
+        decryptResult = playfair(key,en_plain_text, False)
+
+        return JsonResponse({'decrypted_string': decryptResult.lower()})
