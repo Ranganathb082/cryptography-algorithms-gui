dc2821f92630dd642ce2de883fc1e5d2a7c95536
Ranganatth <ibranganatth82@gmail.com>
2023-09-10 07:44:17 +0530

    New files added playfairDecrypt.py, playfairEncrypt.py added views are updated

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from apis.playfair import *
-
-# Create your views here.
-
+from apis.playfairEncrypt import *
+from apis.playfairDecrypt import *
 
 def hello_world(requeset):
     return HttpResponse("Hello World")
+
 
 
 def playFairEncrypt(request):
@@ -14,7 +13,7 @@ def playFairEncrypt(request):
         plain_text = request.GET.get('plain_text', '')
         key = request.GET.get('key', '')
 
-        encryptResult=playfair(key, plain_text)
+        encryptResult=encryptByPlayFair(key, plain_text)
 
         return JsonResponse({'encrypted_string': encryptResult.lower()})
 
@@ -25,6 +24,6 @@ def playFairDecrypt(request):
         # get method takes parameter so use the parameter in the url too
         key = request.GET.get('key', '')
 
-        decryptResult = playfair(key,en_plain_text, False)
+        decryptResult = decryptByPlayFair(key,en_plain_text)
 
         return JsonResponse({'decrypted_string': decryptResult.lower()})
